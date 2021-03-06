4c68d0160aab2dc7e4fbc811904e1faf085f1aa4
Yohcr <yohana.chavez98@gmail.com>
2021-03-05 18:57:56 -0600

    First commit

---
+++ factorial.php
@@ -0,0 +1,15 @@
+<?php
+/*
+	Yohana E. Chavez Reyes
+	05 de Marzo, 2020
+	Descripcion: Codigo que encuentra el factorial de cualquier número natural positivo
+	Entradas:
+		$num => numero natural positivo
+	Salidas:
+		$rest => factorial
+*/
+
+	$num = readline("Ingresa numero: ");
+
+
+?>
